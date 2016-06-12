6c8f25d3ed18c9cea1502042eb77c3e43ad6a4b5
David Thorne <david@cferthorney.com>
2016-06-12 17:10:15 +0100

    Initial commit to 8.x-1.x
    Initial commit to the 8.x-1.x branch

--- src/Controller/mediumPublishController.php
+++ src/Controller/mediumPublishController.php
@@ -1,13 +1,14 @@
 <?php
 /**
  * @file
- * Medium.medium.inc.
+ * mediumPublishControllerphp.
  */
 
+namespace Drupal\medium_publish\Controller;
 /**
  * Handles communication with Medium.
  */
-class MediumDrupal {
+class mediumPublishController {
   private $apiUrl = 'https://api.medium.com/v1/';
 
   protected $clientId;
@@ -24,6 +25,90 @@ class MediumDrupal {
   public function __construct($credentials) {
     $this->setUpCredentials($credentials);
   }
+
+  /**
+ * Menu callback for config overview.
+ */
+function adminOverview() {
+  ctools_include('export');
+
+  $configs = ctools_export_crud_load_all('medium_publish_config');
+
+  $client_id = variable_get('medium_publish_client_id', FALSE);
+  $client_secret = variable_get('medium_publish_client_secret', FALSE);
+  if (!($client_id && $client_secret)) {
+    drupal_set_message(t('Medium integration must be configured for this to work. Please go to <a href="@link">settings</a> and configure it.', array(
+      '@link' => url('admin/config/content/medium/settings'),
+    )), 'warning');
+  }
+
+  $rows = array();
+  foreach ($configs as $config) {
+    $row = array();
+
+    // Style disabled configurations differently.
+    if (!empty($config->disabled)) {
+      $row['class'][] = 'disabled';
+    }
+
+    $row['data']['details'] = drupal_ucfirst($config->instance);
+
+    $operations = array();
+    if (medium_publish_config_access('disable', $config)) {
+      $operations['edit'] = array(
+        'title' => ($config->export_type & EXPORT_IN_DATABASE) ? t('Edit') : t('Override'),
+        'href' => 'admin/config/content/medium/config/' . $config->instance,
+      );
+    }
+    if (medium_publish_config_access('enable', $config)) {
+      $operations['enable'] = array(
+        'title' => t('Enable'),
+        'href' => 'admin/config/content/medium/config/' . $config->instance . '/enable',
+        'query' => array(
+          'token' => drupal_get_token('enable-' . $config->instance),
+        ) + drupal_get_destination(),
+      );
+    }
+    if (medium_publish_config_access('disable', $config)) {
+      $operations['disable'] = array(
+        'title' => t('Disable'),
+        'href' => 'admin/config/content/medium/config/' . $config->instance . '/disable',
+        'query' => array(
+          'token' => drupal_get_token('disable-' . $config->instance),
+        ) + drupal_get_destination(),
+      );
+    }
+
+    if (medium_publish_config_access('delete', $config)) {
+      $operations['delete'] = array(
+        'title' => t('Delete'),
+        'href' => 'admin/config/content/medium/config/' . $config->instance . '/delete',
+      );
+    }
+    $row['data']['operations'] = array(
+      'data' => array(
+        '#theme' => 'links',
+        '#links' => $operations,
+        '#attributes' => array('class' => array('links', 'inline')),
+      ),
+    );
+
+    $rows[$config->instance] = $row;
+  }
+
+  $build['config_table'] = array(
+    '#theme' => 'table',
+    '#header' => array(
+      'type' => t('Type'),
+      'operations' => t('Operations'),
+    ),
+    '#rows' => $rows,
+    '#empty' => t('No content type configs yet'),
+    '#suffix' => '<p>' . t('Only content types with a enabled config defined will have the option to publish to Medium.') . '</p>',
+  );
+
+  return $build;
+}
 
   /**
    * Get the url to authenticate the user to medium.
